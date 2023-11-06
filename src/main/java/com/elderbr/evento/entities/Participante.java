bdd00f3297c04c9bd18844bd7338eb7b752f56d8
ElderBR <elderbarbosaromao@gmail.com>
2023-11-05 21:57:49 -0300

    Refaturando a entidade Participante adicionando equals e hascode

--- src/main/java/com/elderbr/evento/entities/Participante.java
+++ src/main/java/com/elderbr/evento/entities/Participante.java
@@ -3,6 +3,7 @@ package com.elderbr.evento.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -53,5 +54,20 @@ public class Participante {
 
     public Set<Atividade> getAtividades() {
         return atividades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Participante that = (Participante) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
